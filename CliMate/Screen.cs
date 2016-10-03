810b729e198f2dbbe6504a47500f065b4f3de04e
Ben Denison <bdenison@purdue.edu>
2016-10-02 20:37:53 -0400

    Added Classes for drawing and dsiplaying one facet of an icosahedral hex grid.

---
+++ CliMate/Screen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+
+namespace OpenTKGridTest
+{
+    class Screen
+    {
+        public const int WIDTH = 1200;
+        public const int HEIGHT = 800;
+        static void Display() 
+        {
+            GameWindow window = new GameWindow(WIDTH, HEIGHT);
+            Grid hexGrid = new Grid(window);
+
+            window.Run();
+        }
+    }
+}
