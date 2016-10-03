dae235eaca19acfc286d3048b6cd3c6474cd6942
Alex Shelley <ashelley@purdue.edu>
2016-10-03 12:40:02 -0400

    Added a test form for perlin noise.

---
+++ CliMate/PerlinTestForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using LibNoise;
+
+namespace CliMate
+{
+    public partial class PerlinTestForm : Form
+    {
+        public PerlinTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PerlinTestForm_Load(object sender, EventArgs e)
+        {
+            LibNoise.Primitive.
+        }
+    }
+}
