aeb521b6f9eb59b1c34ad72df986739b569b5506
Jason Shipp <shippj@purdue.edu>
2016-10-13 15:52:39 -0400

    Started Notes

---
+++ CliMate/Note.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Note
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public string note { get; set; }
+    public string map { get; set; }
+
+	public Note()
+	{
+	}
+
+    public Note(int x, int y, string note, string map)
+    {
+        this.x = x;
+        this.y = y;
+        this.note = note;
+        this.map = map;
+    }
+    
+    public void SaveNote()
+    {
+
+    }   
+}
