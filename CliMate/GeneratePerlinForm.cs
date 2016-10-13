77e58e6f9abcd9d9aa9ecf0e1fdeb65ce7de0979
Alex Shelley <ashelley@purdue.edu>
2016-10-13 15:35:53 -0400

    Implemented generate button
    Signed-off-by: Alex Shelley <ashelley@purdue.edu>

--- CliMate/GeneratePerlinForm.cs
+++ CliMate/GeneratePerlinForm.cs
@@ -12,6 +12,8 @@ namespace CliMate
 {
     public partial class GeneratePerlinForm : Form
     {
+        private Heightmap previewMap;
+
         public GeneratePerlinForm()
         {
             InitializeComponent();
@@ -20,6 +22,51 @@ namespace CliMate
         private void cancelButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            //Get the data from the textboxes
+            int width;
+            int height;
+            int seed;
+
+            //Catch any errors with the textboxes
+            try
+            {
+                width = int.Parse(widthBox.Text);
+                height = int.Parse(heightBox.Text);
+                seed = int.Parse(seedBox.Text);
+            }
+            catch (FormatException err)
+            {
+                MessageBox.Show("ERROR: width, height, and seed must all be integers.");
+                return;
+            }
+            catch (OverflowException err)
+            {
+                MessageBox.Show("ERROR: either width, height, or seed are too long.");
+                return;
+            }
+
+            //Generate the preview map
+            previewMap = new Heightmap(width, height);
+            PerlinNoise noiseGen = new PerlinNoise(seed);
+            
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    double value = noiseGen.GetValue((double)x, (double)y);
+                    previewMap.SetValue(x, y, value);
+                }
+            }
+
+            //Update the preview picture
+            previewBox.Image = previewMap.ToBitmap();
+
+            //Enable the Okay button
+            OKButton.Enabled = true;
         }
     }
 }
