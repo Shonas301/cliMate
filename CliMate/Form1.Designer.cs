21b7137d84db6c98b47f4eb565099e888efdecfc
Nathan Raine <Nathan Raine>
2016-10-12 21:08:09 -0400

    Things added: -Colorfulness -Menu bar items -Toolbar with 2 buttons -openTK panel -img folder added Note: GUI not yet resizable
    Signed-off-by: Nathan Raine <Nathan Raine>

--- CliMate/Form1.Designer.cs
+++ CliMate/Form1.Designer.cs
@@ -30,26 +30,215 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temperatureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.climatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createBiomesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolPanel = new System.Windows.Forms.Panel();
+            this.toolsLabel = new System.Windows.Forms.Label();
+            this.brushPointerToggle = new System.Windows.Forms.Button();
+            this.regularPointerToggle = new System.Windows.Forms.Button();
+            this.openTKPanel = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.toolPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.generateToolStripMenuItem,
+            this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(1200, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.preferencesToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // preferencesToolStripMenuItem
+            // 
+            this.preferencesToolStripMenuItem.Name = "preferencesToolStripMenuItem";
+            this.preferencesToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.preferencesToolStripMenuItem.Text = "Preferences";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.temperatureToolStripMenuItem,
+            this.climatesToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // temperatureToolStripMenuItem
+            // 
+            this.temperatureToolStripMenuItem.Name = "temperatureToolStripMenuItem";
+            this.temperatureToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.temperatureToolStripMenuItem.Text = "Height Map";
+            // 
+            // climatesToolStripMenuItem
+            // 
+            this.climatesToolStripMenuItem.Name = "climatesToolStripMenuItem";
+            this.climatesToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.climatesToolStripMenuItem.Text = "Climates";
+            // 
+            // generateToolStripMenuItem
+            // 
+            this.generateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createBiomesToolStripMenuItem});
+            this.generateToolStripMenuItem.Name = "generateToolStripMenuItem";
+            this.generateToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.generateToolStripMenuItem.Text = "Generate";
+            // 
+            // createBiomesToolStripMenuItem
+            // 
+            this.createBiomesToolStripMenuItem.Name = "createBiomesToolStripMenuItem";
+            this.createBiomesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.createBiomesToolStripMenuItem.Text = "Create Biomes";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // toolPanel
+            // 
+            this.toolPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolPanel.Controls.Add(this.toolsLabel);
+            this.toolPanel.Controls.Add(this.brushPointerToggle);
+            this.toolPanel.Controls.Add(this.regularPointerToggle);
+            this.toolPanel.Location = new System.Drawing.Point(0, 24);
+            this.toolPanel.Name = "toolPanel";
+            this.toolPanel.Size = new System.Drawing.Size(130, 576);
+            this.toolPanel.TabIndex = 1;
+            // 
+            // toolsLabel
+            // 
+            this.toolsLabel.AutoSize = true;
+            this.toolsLabel.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolsLabel.ForeColor = System.Drawing.Color.Silver;
+            this.toolsLabel.Location = new System.Drawing.Point(4, 4);
+            this.toolsLabel.Name = "toolsLabel";
+            this.toolsLabel.Size = new System.Drawing.Size(48, 23);
+            this.toolsLabel.TabIndex = 2;
+            this.toolsLabel.Text = "Tools";
+            // 
+            // brushPointerToggle
+            // 
+            this.brushPointerToggle.BackColor = System.Drawing.Color.DarkGray;
+            this.brushPointerToggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.brushPointerToggle.ForeColor = System.Drawing.Color.Gray;
+            this.brushPointerToggle.Image = ((System.Drawing.Image)(resources.GetObject("brushPointerToggle.Image")));
+            this.brushPointerToggle.Location = new System.Drawing.Point(70, 30);
+            this.brushPointerToggle.Name = "brushPointerToggle";
+            this.brushPointerToggle.Size = new System.Drawing.Size(50, 50);
+            this.brushPointerToggle.TabIndex = 1;
+            this.brushPointerToggle.UseVisualStyleBackColor = false;
+            // 
+            // regularPointerToggle
+            // 
+            this.regularPointerToggle.BackColor = System.Drawing.Color.DarkGray;
+            this.regularPointerToggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.regularPointerToggle.ForeColor = System.Drawing.Color.Gray;
+            this.regularPointerToggle.Image = ((System.Drawing.Image)(resources.GetObject("regularPointerToggle.Image")));
+            this.regularPointerToggle.Location = new System.Drawing.Point(10, 30);
+            this.regularPointerToggle.Name = "regularPointerToggle";
+            this.regularPointerToggle.Size = new System.Drawing.Size(50, 50);
+            this.regularPointerToggle.TabIndex = 0;
+            this.regularPointerToggle.UseVisualStyleBackColor = false;
+            // 
+            // openTKPanel
+            // 
+            this.openTKPanel.Location = new System.Drawing.Point(130, 24);
+            this.openTKPanel.Name = "openTKPanel";
+            this.openTKPanel.Size = new System.Drawing.Size(1070, 576);
+            this.openTKPanel.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.openTKPanel);
+            this.Controls.Add(this.toolPanel);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "CliMate";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolPanel.ResumeLayout(false);
+            this.toolPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +247,25 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem preferencesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem temperatureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem climatesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createBiomesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Panel toolPanel;
+        private System.Windows.Forms.Panel openTKPanel;
+        private System.Windows.Forms.Button regularPointerToggle;
+        private System.Windows.Forms.Label toolsLabel;
+        private System.Windows.Forms.Button brushPointerToggle;
     }
 }
 
--- CliMate/Form1.cs
+++ CliMate/Form1.cs
@@ -16,5 +16,6 @@ namespace CliMate
         {
             InitializeComponent();
         }
+
     }
 }
