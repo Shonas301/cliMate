72c1fe697ec40255d744e10255cb5888545c15eb
Alex Shelley <ashelley@purdue.edu>
2016-11-03 12:18:34 -0400

    Got started on the revamped canvas class. 	I'm writing it in such a way that moving to OpenTK is easy.

--- CliMate/Canvas.cs
+++ CliMate/Canvas.cs
@@ -12,9 +12,37 @@ namespace CliMate
 {
     public partial class Canvas : Form
     {
+        private const int DEFAULT_SIZE = 100;
+
+        private Heightmap image = new Heightmap(DEFAULT_SIZE, DEFAULT_SIZE);
+
         public Canvas()
         {
             InitializeComponent();
+        }
+
+        private void OnGameTick(MouseState mouse, double deltaTime)
+        {
+            //
+        }
+    }
+
+    public class MouseState
+    {
+        public bool leftButton;
+        public bool rightButton;
+        public bool middleButton;
+
+        public int x;   //In the heightmap's coordinate space
+        public int y;   //In the heightmap's coordinate space
+
+        public MouseState(int x, int y, bool leftButton, bool middleButton, bool rightButton)
+        {
+            this.x = x;
+            this.y = y;
+            this.leftButton = leftButton;
+            this.rightButton = rightButton;
+            this.middleButton = middleButton;
         }
     }
 }
