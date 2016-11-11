d9027e0cb9965b0f0594749d7e0242891438c03f
Ben Denison <bdenison@purdue.edu>
2016-11-11 15:43:08 -0500

    Messing with nodes

--- CliMate/RectGrid.cs
+++ CliMate/RectGrid.cs
@@ -1,26 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CliMate
 {
-    [Serializable]
-    public class RectGrid
+    namespace CliMate
     {
-        Tile[][] grid;
-        public RectGrid(int xres, int yres)
+        [Serializable]
+        public class RectGrid
         {
-            for (int i = 0; i < yres; i++)
+            Tile[][] grid;
+            private int width { get; }
+            private int height { get; }
+            //Yeah, you can make this whatever size you want, but this should always be projectWidth/Height
+            //I just found it weird to have a project as a parameter. Project settings? Huh...
+            //Maybe we should leave it as is anyway. Flexibility and all that jazz.
+            public RectGrid(int xres, int yres)
             {
-                grid[i] = new Tile[xres];
-                for (int j = 0; j < xres; j++)
+                for (int x = 0; x < xres; x++)
                 {
-                    grid[i][j] = new Tile(System.Drawing.Color.Black);
+                    grid[x] = new Tile[yres];
+                    for (int y = 0; y < yres; y++)
+                    {
+                        grid[x][y] = new Tile(0);
+                    }
                 }
+                this.width = xres;
+                this.height = yres;
+            }
+
+            public void setTile(int x, int y, float val)
+            {
+                grid[x][y].Value = val;
+            }
+            public Tile getTile(int x, int y)
+            {
+                return grid[x][y];
+            }
+            public Bitmap gridToBitmap()
+            {
+                Bitmap img = new Bitmap(grid.Length, grid[0].Length);
+                for (int x = 0; x < grid.Length; x++)
+                {
+                    for (int y = 0; y < grid[x].Length; y++)
+                    {
+                        img.SetPixel(x, y, grid[x][y].getColor());
+                    }
+                }
+                return img;
             }
         }
-
     }
 }
--- CliMate/Tile.cs
+++ CliMate/Tile.cs
@@ -8,24 +8,31 @@ using System.Drawing;
 
 namespace CliMate
 {
-    [Serializable]
+    
     //I figure this'll be a superclass at some point if we want tiles to hang on to other data such as wind vectors or something.
-    public class Tile
-    {
-        private Color color;
+    namespace CliMate {
+        [Serializable]
+        public class Tile
+        {
+            private float val;
 
-        public Tile(Color color)
-        {
-            this.color = color;
-        }
+            public float Value
+            {
+                get { return val; }
+                set { val = value; }
+            }
 
-        public Color getColor()
-        {
-            return color;
-        }
-        public void setColor(Color color)
-        {
-            this.color = color;
+
+            public Tile(float val)
+            {
+                this.val = val;
+            }
+
+            public Color getColor()
+            {
+                int iVal = (int)(this.val * 255);
+                return Color.FromArgb(iVal, iVal, iVal);
+            }
         }
     }
 }
